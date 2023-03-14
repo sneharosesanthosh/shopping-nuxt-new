2f51fed7de39f27495d198579f6272a399c8b917
sneha <inbox.sneha.santhosh@gmail.com>
2023-03-14 13:56:20 +0530

    productService file

---
+++ services/ProductService.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+const apiClient = axios.create({
+    baseURL: 'https://fakestoreapi.com',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    withCredentials: false
+})
+
+export default {
+    getProducts() {
+        console.log("inside getProducts");
+        return apiClient.get("/products");
+    },
+
+}
